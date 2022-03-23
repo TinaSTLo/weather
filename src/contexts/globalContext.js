0672f7a8e87c8618e9406028d37a94792330c04e
TinaSTLo <91168428+TinaSTLo@users.noreply.github.com>
2022-03-23 13:45:05 +0800

    Add localStorage for weather page

--- src/contexts/globalContext.js
+++ src/contexts/globalContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useRef } from 'react';
+import React, { createContext, useContext, useState, useRef, useEffect } from 'react';
 import useRwd from 'src/hooks/useRwd';
 
 // Context API
@@ -13,10 +13,19 @@ export const useGlobalStore = () => useContext(Context);
  * @returns {JSX.Element}   JSX
  */
 export const GlobalContextProvider = ({ children }) => {
-    const [todos, setTodos] = useState([]); // Todo item Array
     const id = useRef(1); // set 1 with start
 
+    const storageTodoList = localStorage.getItem('todoList') ? JSON.parse(localStorage.getItem('todoList')) : []; // LocalStorage get todoList
+    const [todos, setTodos] = useState(storageTodoList || []); // Current city name
+
     const rwdMode = useRwd(); // RWD
+
+    /**
+     * localStorage set todoList
+     */
+    useEffect(() => {
+        localStorage.setItem('todoList', JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <Context.Provider
